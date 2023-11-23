1df868a15c29b1b54173b62436ed52cac09b3f1c
Tiagofrik <bicalho.franck.tiago@gmail.com>
2023-11-23 18:43:55 -0300

    Startup.cs

---
+++ DoarFazBem_Api/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OpenApi.Models;
+
+
+namespace DoarFazBem_Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DoarFazBem-Api", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, Microsoft.Extensions.Hosting.IHostingEnvironment env)
+        {
+            // Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "DoarFazBem-Api");
+            });
+        }
+    }
+}
