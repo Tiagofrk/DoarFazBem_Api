a7c73fbbbca957853297fe4ef2fbd5339ec54d88
Tiagofrik <bicalho.franck.tiago@gmail.com>
2023-11-23 18:37:30 -0300

    remove gen

--- DoarFazBem_Api/Program.cs
+++ DoarFazBem_Api/Program.cs
@@ -21,7 +21,6 @@ else
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddSwaggerGen(c =>
 {
